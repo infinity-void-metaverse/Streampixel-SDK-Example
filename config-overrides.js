6bcc46cb1340030b5946b31d850b1061f9d6e101
Infinityvoid2021 <104661984+Infinityvoid2021@users.noreply.github.com>
2025-04-16 16:50:46 +0530

    Update

--- config-overrides.js
+++ config-overrides.js
@@ -13,14 +13,18 @@ module.exports = function override(config) {
         "url": require.resolve("url"),
         "zlib": require.resolve("browserify-zlib"),
         "path": require.resolve("path-browserify"),
-    })
+        "process": require.resolve("process/browser")  
+    });
     config.resolve.fallback = fallback;
+
     config.plugins = (config.plugins || []).concat([
         new webpack.ProvidePlugin({
             process: 'process/browser',
             Buffer: ['buffer', 'Buffer']
         })
-    ])
+    ]);
+
     config.ignoreWarnings = [/Failed to parse source map/];
+
     return config;
 }
